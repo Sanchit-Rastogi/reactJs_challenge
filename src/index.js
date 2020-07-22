5ef15d4d5f69703802901621c594e931a67dfb58
Sanchit Rastogi <34184127+Sanchit-Rastogi@users.noreply.github.com>
2020-07-22 15:22:55 +0530

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+var time = new Date();
 
-const rootElement = document.getElementById("root");
+var message = "";
+var msgStyle = {
+  color: "blue"
+};
+
+if (time.getHours() < 12) {
+  message = "Good Morning";
+  msgStyle.color = "pink";
+} else if (time.getHours() < 15) {
+  message = "Good Afternoon";
+  msgStyle.color = "teal";
+} else {
+  message = "Good Evening";
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
+  <div>
+    <h1>React Js Challenge</h1>
+    <h2 className="heading" style={msgStyle}>
+      {message}
+    </h2>
+  </div>,
+  document.getElementById("root")
 );
